67972f1289fca5bc9ea17c6b2deb06fb4a1769a8
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-02-03 19:52:29 +0000

    refactor

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -12,11 +12,7 @@ public static class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
-        var services = builder.Services;
-        var configuration = builder.Configuration;
-        
-        ConfigureServices(services, configuration);
+        ConfigureServices(builder.Services, builder.Configuration);
 
         var app = builder.Build();
         ConfigureApp(app);
