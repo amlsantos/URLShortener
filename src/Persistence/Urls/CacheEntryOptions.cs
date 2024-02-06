7274aec6a74e5f02cc59f501208c8faf2517c13f
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-02-06 18:51:54 +0000

    fixed feature envy

--- src/Persistence/Urls/CacheEntryOptions.cs
+++ src/Persistence/Urls/CacheEntryOptions.cs
@@ -1,9 +1,23 @@
-﻿namespace Persistence.Urls;
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Persistence.Urls;
 
 public class CacheEntryOptions
 {
     public const string MemoryCacheEntry = nameof(MemoryCacheEntry);
     public int SlidingExpiration { get; set; }
-    public int AbsoluteExpiration { get; set; }
-    public string CacheItemPriority { get; set; }
+    public int AbsoluteExpiration { get; set;}
+    public string CachePriority { get; set;}
+
+    public MemoryCacheEntryOptions ToMemoryOptions()
+    {
+        var isValidPriority = Enum.TryParse(CachePriority, out CacheItemPriority priority);
+        if (!isValidPriority)
+            priority = CacheItemPriority.Normal;
+
+        return new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromSeconds(SlidingExpiration))
+            .SetAbsoluteExpiration(TimeSpan.FromSeconds(AbsoluteExpiration))
+            .SetPriority(priority);
+    }
 }
