47e4d4d400be051949cd1a264254a03166806ff7
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-02-09 14:17:39 +0000

    improved JwtBearerOptionsSetup

--- src/Infrastructure/Users/Configurations/JwtBearerOptionsSetup.cs
+++ src/Infrastructure/Users/Configurations/JwtBearerOptionsSetup.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text;
+using Application.Interfaces;
 using Infrastructure.Exceptions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
@@ -30,18 +32,52 @@ public class JwtBearerOptionsSetup : IConfigureNamedOptions<JwtBearerOptions>
 
         options.Events = new JwtBearerEvents
         {
-            OnChallenge = WriteAsync
+            OnForbidden = context => HandleForbidden(context),
+            OnChallenge = context => HandleChallenge(context),
+            OnAuthenticationFailed = context => HandleAuthenticationFailed(context)
         };
     }
 
-    private static Task WriteAsync(JwtBearerChallengeContext context)
+    private static Task HandleForbidden(ForbiddenContext context)
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        context.Response.ContentType = "application/json";
+        
+        throw new AuthenticationException("Forbidden", "You dont have permissions to access this resource");
+    }
+
+    private static Task HandleChallenge(JwtBearerChallengeContext context)
     {
         context.HandleResponse();
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        context.Response.ContentType = "application/json";
 
-        if (!string.IsNullOrEmpty(context.Error))
-            throw new AuthenticationException(context.Error, context.ErrorDescription ?? string.Empty);
+        // Ensure we always have an error and error description.
+        if (string.IsNullOrEmpty(context.Error))
+            context.Error = "invalid_token";
+        if (string.IsNullOrEmpty(context.ErrorDescription))
+            context.ErrorDescription = "This request requires a valid JWT access token to be provided";
 
-        return Task.CompletedTask;
+        // Add some extra context for expired tokens.
+        if (context.AuthenticateFailure is not null && context.AuthenticateFailure.GetType() ==
+            typeof(SecurityTokenExpiredException))
+        {
+            var inner = context.AuthenticateFailure as SecurityTokenExpiredException;
+            context.Response.Headers.Add("x-token-expired", inner.Expires.ToString("o"));
+            context.ErrorDescription = $"The token expired on {inner.Expires.ToString("o")}";
+            
+            throw new AuthenticationException(context.Error, context.ErrorDescription, inner);
+        }
+
+        throw new AuthenticationException(context.Error, context.ErrorDescription);
+    }
+
+    private Task HandleAuthenticationFailed(AuthenticationFailedContext context)
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        context.Response.ContentType = "application/json";
+        
+        throw new AuthenticationException("Invalid token", context.Exception.Message);
     }
 
     public void Configure(string? name, JwtBearerOptions options) => Configure(options);
